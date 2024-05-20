3f19e90061d7732091757ac0b7fc27d86e5e135a
S.Ivanov <trollcraft1002@gmail.com>
2024-05-20 03:26:55 +0300

    fixed collection(misspelled with collator instead of collection)

--- src/main/java/com/example/mongodb/model/Product.java
+++ src/main/java/com/example/mongodb/model/Product.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collation = "products")
+@Document(collection = "products")
 public class Product {
 @Id
 private String id;
--- src/main/java/com/example/mongodb/model/Supplier.java
+++ src/main/java/com/example/mongodb/model/Supplier.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collation  = "suppliers")
+@Document(collection  = "suppliers")
 public class Supplier {
     @Id
     private String id;
